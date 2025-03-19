33a44a84d14d9303bcaa678195b0617a24137cb4
parker <54675124+Lourdez@users.noreply.github.com>
2025-03-19 16:56:17 +0530

    Developed Spring RESTful

---
+++ src/main/java/com/hello/helloworld/HelloworldController.java
@@ -0,0 +1,14 @@
+package com.hello.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class HelloworldController {
+	@GetMapping("helloworld")
+	public String gethelloworld() {
+		return "Hello World";
+	}
+}
